27d4909e5e4d27c73a17d51ac1fff3b5b5665696
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-10-18 23:18:43 +0700

    menambahkan fitur hapus dan tambah data #10

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,20 +1,54 @@
 
 <div class="container">
-	<div class="row mt-3">
-		<div class="col-md-6">
+    <?php if ($this->session->flashdata('flash')) : ?>
+	    <div class="row mt-3">
+	        <div class="col-md-6">
+	            <div class="alert alert-success alert-dismissible fade show" role="alert">
+	                Data mahasiswa <b>berhasil</b> <?= $this->session->flashdata('flash'); ?>.
+	                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+	                    <span aria-hidden="true">&times;</span>
+	                </button>
+	            </div>
+	        </div>
+	    </div> 
+    <?php endif; ?>
+  		<div class="row">
+  			<div class="col-md-6">
 			<a href="<?php base_url(); ?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>
 		</div>
-	</div>
-	<div class="row mt-3">
-
-		<div class="col-md-6">
-			<h3>Daftar Mahasiswa</h3>
-			<ul class="list-group ">
-				<?php foreach ($mahasiswa as $mhs) :?>
-			  		<li class="list-group-item"><?=$mhs['nama']; ?></li>
-			  	<?php endforeach; ?>
-			 
-			</ul>
-		</div>
-	</div>
-</div>
+  			<div class="col-10">
+  				<h3>Daftar Mahasiswa</h3>
+				<table class="table">
+				  <thead class="thead-light">
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">Nama</th>
+				      <th scope="col">NRP</th>
+				      <th scope="col">E-mail</th>
+				      <th scope="col">Jurusan</th>
+				      <th scope="col" ><center>Menu</center></th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				  	<?php $i=0; ?>
+				  	<?php foreach ($mahasiswa as $mhs) : ?>
+				    <tr>
+				    	
+				      <th scope="row"><?php  echo $i=$i+1;?></th>
+				      <td><?= $mhs['nama']; ?></td>
+				      <td><?= $mhs['nrp']; ?></td>
+				      <td><?= $mhs['email']; ?></td>
+				      <td><?= $mhs['jurusan']; ?></td>
+				      <td>
+				      	<a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right ml-1" onclick="return confirm('apakah anda yakin ingin menghapus data ini ?')">Hapus</a>
+				      	<a href="" class="badge badge-success float-right ml-1">Ubah</a>
+				      	<a href="" class="badge badge-primary float-right ml-1">Detail</a>
+				      </td>
+				    </tr>
+					<?php endforeach ?>
+				 </tbody>
+				</table> 
+  			</div>
+  		</div>
+  	</div>
+  	
--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -1,38 +1,47 @@
 <div class="container">
-	<div class="row mt-3">
-		<div class="col-md-6">
-			<div class="card">
-			  <div class="card-header">
-			    Form Tambah Data mahasiswa
-			  </div>
-			  <div class="card-body">
-			  		
-			  		<form action="" method="post">
-						 <div class="form-group">
-						    <label for="nama">Nama </label>
-						    <input type="text" name="nama" class="form-control" id="nama" >
-						  </div>
-						  <div class="form-group">
-						    <label for="nrp">NRP </label>
-						    <input type="text" name="nrp" class="form-control" id="nrp" >
-						  </div>
-						  <div class="form-group">
-						    <label for="email">E-mail </label>
-						    <input type="email" name="email" class="form-control" id="email" >
-						  </div>
-						  <div class="form-group">
-						    <label for="jurusan">Jurusan</label>
-						    <select class="form-control" id="jurusan">
-						      <option value="Teknik Informatika">Teknik Informatika</option>
-						      <option value="Teknik Mesin">Teknik Mesin</option>
-						      <option value="Teknik Pangan">Teknik Mesin</option>		      
-						    </select>
-						  </div>
-						  <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
-					</form>
-			  </div>
-			</div>
-			
-		</div>		
-	</div>
+
+    <div class="row mt-3">
+        <div class="col-md-6">
+
+            <div class="card">
+                <div class="card-header">
+                    Form Tambah Data Mahasiswa
+                </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" name="nama" class="form-control" id="nama">
+                            <small class="form-text text-danger"><?= form_error('nama'); ?></small>
+                        </div>
+                        <div class="form-group">
+                            <label for="nrp">NRP</label>
+                            <input type="text" name="nrp" class="form-control" id="nrp">
+                            <small class="form-text text-danger"><?= form_error('nrp'); ?></small>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" class="form-control" id="email">
+                            <small class="form-text text-danger"><?= form_error('email'); ?></small>
+                        </div>
+                        <div class="form-group">
+                            <label for="jurusan">Jurusan</label>
+                            <select class="form-control" id="jurusan" name="jurusan">
+                                <option value="Teknik Informatika">Teknik Informatika</option>
+                                <option value="Teknik Industri">Teknik Industri</option>
+                                <option value="Teknik Pangan">Teknik Pangan</option>
+                                <option value="Teknik Mesin">Teknik Mesin</option>
+                                <option value="Teknik Planologi">Teknik Planologi</option>
+                                <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                            </select>
+                        </div>
+                        <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
+                    </form>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
 </div>
